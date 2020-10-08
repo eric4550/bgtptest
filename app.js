77977b094f9909db2bb0354b0fe754cc71684691
Parker <perickson@csumb.edu>
2020-10-07 21:16:38 -0700

    first

---
+++ app.js
@@ -0,0 +1,76 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+
+//added-----------
+const mysql = require('mysql');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+// var loginRouter = require('./routes/pages/login');
+// var cartRouter = require('./routes/cart');
+// var productsRouter = require('./routes/products');
+// var profileRouter = require('./routes/profile');
+// var postRouter = require('./routes/post');
+// var signupRouter = require('./routes/signup');
+
+var app = express();
+
+//didnt add:
+//database
+//port wiring
+//opening server and opening listening channel
+var server = app.listen(8081, function() {
+  //opens server on port 3000, does stuff
+});
+
+//--------------------
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+//ADDED-----------
+app.use('/', indexRouter);
+// app.use('/login',loginRouter);
+app.use('/users', usersRouter);
+// app.use('/cart', cartRouter);
+// app.use('/post', postRouter);
+// app.use('/products',productsRouter);
+// app.use('/signup',signupRouter);
+// app.use('/profile',profileRouter);
+//---------
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
